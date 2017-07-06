29281a39986dd54eeadc87aa2d03209a3fbafac3
Defilan <defilan@gmail.com>
2017-07-06 11:56:55 -0700

    Removed old dependencies

--- metadata.rb
+++ metadata.rb
@@ -8,7 +8,6 @@ version '0.1.1'
 chef_version '>= 12.1' if respond_to?(:chef_version)
 depends 'homebrew'
 depends 'build-essential'
-depends 'oh_my_zsh'
 
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
